fa0fc1eff3d26a51129efe82bbdafb7c7b6916b2
edin <edo.truman@gmail.com>
2021-04-21 21:13:02 +0200

    second switch

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,34 @@ import React from "react";
 import "../app.css";
 import Bulb from "./Bulb.js";
 import Switch from "./Switch.js";
+import Switch2 from "./Switch2.js";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      switchOn: false
+      switchOn: false,
+      switch2On: false
     };
 
     this.toggleSwitch = this.toggleSwitch.bind(this);
+    this.toggleSwitch2 = this.toggleSwitch2.bind(this);
   }
 
   toggleSwitch() {
     this.setState({ switchOn: !this.state.switchOn });
+  }
+  toggleSwitch2() {
+    this.setState({ switch2On: !this.state.switch2On });
   }
 
   render() {
     return (
       <div>
         <Switch isOn={this.state.switchOn} onSwitched={this.toggleSwitch} />
-        <Bulb isOn={this.state.switchOn} />
+        <Switch2 isOn={this.state.switch2On} onSwitched={this.toggleSwitch2} />
+        <Bulb isOn={this.state.switchOn && this.state.switch2On} />
       </div>
     );
   }
